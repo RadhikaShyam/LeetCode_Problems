181e0d0981e1dc98bf502a5f477192f20288abc7
Deeksha <deedeeksha.shukla@outlook.com>
2025-08-02 22:53:20 +0530

    final changes

--- ThreeSum.cs
+++ ThreeSum.cs
@@ -44,7 +44,7 @@ public class Solution_8
             {
                 result.Add(new List<int> {nums[i],nums[left],nums[right]});
                 while(left<right && nums[left]==nums[left+1])left++;
-                while(left<right && nums[left]==nums[right-1])right--;
+                while(left<right && nums[right]==nums[right-1])right--;
                 left++;
                 right--;
             }
